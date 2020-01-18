28f83b8b2e436620d5e299c7e54579abdfffd4b5
Moj <maghrout@googlemail.com>
2020-01-18 14:49:55 +0000

    move things aroiund

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -2,12 +2,11 @@ const PrimeFactors = function() {};
 
 PrimeFactors.prototype.of = function(primeNumber) {
     var primeFactorArray = [];
-
-        for(var divisor = 2; divisor <= primeNumber; ++divisor) {
-            for(;primeNumber % divisor === 0; primeNumber /= divisor) {
-                primeFactorArray.push(divisor);
-            }
+    for(var divisor = 2; divisor <= primeNumber; ++divisor) {
+        for(;primeNumber % divisor === 0; primeNumber /= divisor) {
+            primeFactorArray.push(divisor);
         }
+    }
     return primeFactorArray;
 }
 
